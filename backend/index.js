9268113d9a60f94cd7b04178d0eb805e8d6d61ed
JacobDiEdwardo <jacob.diedwardo@gmail.com>
2019-03-12 14:45:05 -0400

    installed some extra packages to prepare for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
 
 const SERVER_CONFIGS = require('./constants/server');
 
@@ -9,6 +11,9 @@ const app = express();
 
 configureServer(app);
 configureRoutes(app);
+
+app.use(helmet());
+app.use(compression());
 
 app.listen(SERVER_CONFIGS.PORT, error => {
     if (error) throw error;
