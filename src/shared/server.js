f5cc4e802e420ddd80a936a9b50fe839b399c2d2
JacobDiEdwardo <jacob.diedwardo@gmail.com>
2019-03-14 15:37:06 -0400

    added correct url for my server

--- src/shared/server.js
+++ src/shared/server.js
@@ -1,5 +1,5 @@
 const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'production'
-    ? 'http://myapidomain.com'
+    ? 'https://the-fabio-pizza-payment-api.herokuapp.com/'
     : 'http://localhost:8080';
 
 export default PAYMENT_SERVER_URL;
